0ce3e345ee38c3138422b653bca05efaaed66f69
Oguzhan <s_celiko@stud.hwr-berlin.de>
2019-02-06 17:08:59 +0100

    Beiträge erstellen

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
-Rails.application.routes.draw do
-  get 'home/index'
+# config/routes.rb
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  Rails.application.routes.draw do
+    get 'post/create'
+    post 'post/store'
+
+    root 'home#index'
+  end
